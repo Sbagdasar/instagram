0d3593315a6d871ae8650fed71e07b29d3245ab0
Samvel <samvelbagdasaryan1@gmail.com>
2020-12-17 12:29:33 +0300

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ButtonComponent } from './button/button.component';
+import { ButtonsComponent } from './buttons/buttons.component';
+import { PostComponent } from './post/post.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PostFooterComponent } from './post/post-footer/post-footer.component';
+import { PostBodyComponent } from './post/post-body/post-body.component';
+import { PostHeaderComponent } from './post/post-header/post-header.component';
+import { FooterButtonsComponent } from './post/post-footer/footer-buttons/footer-buttons.component';
+import { FooterCommentsComponent } from './post/post-footer/footer-comments/footer-comments.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ButtonComponent
+    ButtonsComponent,
+    PostComponent,
+    PostFooterComponent,
+    PostBodyComponent,
+    PostHeaderComponent,
+    FooterButtonsComponent,
+    FooterCommentsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
